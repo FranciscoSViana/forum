d9f4a2524d048ea6b51442f663869bbcc3143ef4
Francisco Viana <franciscosantos29@gmail.com>
2022-05-17 15:25:06 -0300

    implementação do Spring security para acesso aos endpoints e acesso a usuários

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$dYGCZMFPERyh.ZzM9gwJWugKBHQE6ZY65BOQGNXk76eAxVwo58eqS');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
